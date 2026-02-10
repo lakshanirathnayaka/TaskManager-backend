a3f4348930f46a1542c0afa9346b57da729c0219
lakshanirathnayaka <153893654+lakshanirathnayaka@users.noreply.github.com>
2026-02-10 23:21:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
-const { Pool } = require("pg");
+// db.js
+const { Pool } = require('pg');
 
+// Create a connection pool
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "PSQL123@",
-  db: "postgres",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+// Test the connection
+pool.on('connect', () => {
+  console.log('Connected to Neon PostgreSQL database');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+// Export the pool for use in other files
 module.exports = pool;
